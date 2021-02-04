7ca5fafec27f36f673df30332ce51aa87033f7fc
Shreyansu Raj <sraj221099@gmail.com>
2021-02-04 09:36:39 +0530

    Top Down Approach

---
+++ InterviewBit/Dynamic Programming/Longest common subsequenc.cpp
@@ -0,0 +1,28 @@
+public class Solution 
+{
+  
+    public int solve(String A, String B) 
+    {
+        int n=A.length();
+        int m=B.length();
+        int dp[][] =new int[n+1][m+1];
+        // for(int i=0;i<n;i++)
+        //     Arrays.fill(dp[i][0],0);
+        // for(int j=0;j<m;j++)
+        //     Arrays.fill(dp[0][i],0);
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(A.charAt(i-1)==B.charAt(j-1))
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        
+        return dp[n][m];
+        
+    }
+}

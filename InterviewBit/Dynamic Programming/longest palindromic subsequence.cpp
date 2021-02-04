bd171c1bb1827c37e05cfbfe19789d9e0b031a76
Shreyansu Raj <sraj221099@gmail.com>
2021-02-04 10:26:19 +0530

    Top Down Approach

---
+++ InterviewBit/Dynamic Programming/longest palindromic subsequence.cpp
@@ -0,0 +1,37 @@
+int Solution::solve(string s) 
+{
+    string w=s;
+        
+        reverse(s.begin(),s.end());
+        
+        
+        
+        int n=s.size();
+        int dp[n+1][n+1];
+        memset(dp,0,sizeof(dp));
+        for(int i=0;i<=n;i++)
+            dp[i][0]=0;
+        for(int i=0;i<=n;i++)
+            dp[0][i]=0;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s[i-1]==w[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        // for(int i=0;i<=n;i++)
+        // {
+        //     for(int j=0;j<=n;j++)
+        //     {
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return dp[n][n];
+        
+}

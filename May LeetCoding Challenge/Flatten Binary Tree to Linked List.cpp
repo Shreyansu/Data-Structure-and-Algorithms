4f536bd8a6c9a7dde5f3cd62ff09b343d95b4c62
Shreyansu Raj <sraj221099@gmail.com>
2021-05-20 22:11:10 +0530

    Day 14

---
+++ May LeetCoding Challenge/Flatten Binary Tree to Linked List.cpp
@@ -0,0 +1,36 @@
+
+class Solution 
+{
+public:
+    void flat(TreeNode* node)
+    {
+        if(node==NULL)
+            return;
+        if(node->left)
+        {
+            TreeNode* x=node->left;
+            while(x->right!=NULL)
+                x=x->right;
+            if(node->right)
+            {
+                TreeNode* temp=node->right;
+                node->right=node->left;
+                x->right=temp;
+            }
+            else
+            {
+                node->right=node->left;
+            }
+            node->left=NULL;
+        }
+        flat(node->right);
+    }
+    void flatten(TreeNode* root) 
+    {
+        if(root==NULL)
+            return;
+        
+        TreeNode* temp=root;
+        flat(temp);  
+    }
+};
